34cc73ddf1807fe38953e5a429788ae1abfb4ac5
Joaquin S <joaquin050416@gmail.com>
2021-09-15 14:22:38 -0500

    Segundo commit js

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function sumarNumeros(numero1, numero2) {
   suma = number1 + number2;
   document.getElementById('resultado').value = suma;
 }
-//hello
+function restarNumeros(numero1, numero2) {
+  let number1 = parseFloat(numero1);
+  let number2 = parseFloat(numero2);
+  let resta;
+  suma = number1 - number2;
+  document.getElementById('resultado').value = suma;
+}
