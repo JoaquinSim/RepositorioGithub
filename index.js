431553d8472491f24f88939b4187e0ebe1e33df8
Joaquin S <joaquin050416@gmail.com>
2021-09-15 11:26:41 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function sumarNumeros(numero1, numero2) {
+  let number1 = parseFloat(numero1);
+  let number2 = parseFloat(numero2);
+  let suma;
+  suma = number1 + number2;
+  document.getElementById('resultado').value = suma;
+}
+//
