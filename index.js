89b6a628e96f0ad3cb7511dd0018feb950ca53a9
Joaquin Simbaña <90775194+JoaquinSim@users.noreply.github.com>
2021-09-15 14:25:41 -0500

    Division

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ function sumarNumeros(numero1, numero2) {
   suma = number1 + number2;
   document.getElementById('resultado').value = suma;
 }
+
 function restarNumeros(numero1, numero2) {
   let number1 = parseFloat(numero1);
   let number2 = parseFloat(numero2);
   let resta;
   suma = number1 - number2;
+  document.getElementById('resultado').value = suma;
+}
+
+function dividirNumeros(numero1, numero2) {
+  let number1 = parseFloat(numero1);
+  let number2 = parseFloat(numero2);
+  let resta;
+  suma = number1 / number2;
   document.getElementById('resultado').value = suma;
 }
